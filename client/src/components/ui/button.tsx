5dcb00449e989da7ddaac1750519d20c89dcaa58
maria <118808220+yum3lo@users.noreply.github.com>
2025-08-02 18:33:49 +0300

    fix(button): erase focus ring to "none" button variant

--- client/src/components/ui/button.tsx
+++ client/src/components/ui/button.tsx
@@ -19,7 +19,7 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
-        none: "bg-transparent shadow-none hover:bg-transparent",
+        none: "bg-transparent shadow-none hover:bg-transparent focus-visible:ring-0",
       },
       size: {
         default: "h-9 px-4 py-2",
